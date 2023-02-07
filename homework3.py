ee4915d94faf14644815b02b9ea8eaa69e20f7dd
Kate Sankovich <katya.sankovich@gmail.com>
2023-02-07 21:03:20 +0200

    add homework and practice for lesson 3

---
+++ homework3.py
@@ -0,0 +1,34 @@
+# Домашнее задание
+
+int1 = 1.6
+int2 = 2.99
+int_round1 = round(int1)
+int_round2 = round(int2)
+print(int_round1)
+print(int_round2)
+
+str = 'www.my_site.com#about'
+print(str.replace('#', '/'))
+
+word = 'stroka'
+print(word + 'ing')
+
+stroka = 'Ivanou Ivan'
+print(stroka[7:11] + ' ' + stroka[0:6])
+
+animal = ' cat '
+print(animal.lstrip().rstrip())
+
+school = {'1a': 20, '2b': 19, '3f': 25, '4t': 21, '5v': 22, '6j': 22, '7k': 18, '8c': 21, '9n': 23, '10s': 25}
+print(type(school))
+
+lst = [1, 2, 3, 9]
+print(lst[1])
+
+str1 = 'employ'
+str2 = 'employment'
+print(str1 in str2)
+
+x = "My name is Agent Smith"
+print(x[1])  # y
+print(x[3:16:3])  # nesgt
---
+++ practice3.py
@@ -0,0 +1,22 @@
+# Практика
+
+int = 7
+flt = 3.14
+str = "Dog"
+lst = [1, 5, 8]
+dic = {'one': 1, 'two': 2}
+tup = ('my', 'name', 'is')
+st = {'cat', 'cow', 'duck'}
+bln = True
+emp = None
+
+print(type(int))
+print(type(flt))
+print(type(str))
+print(type(lst))
+print(type(dic))
+print(type(tup))
+print(type(st))
+print(type(bln))
+print(type(emp))
+
